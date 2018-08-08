b59159f8407aa7024b105c7c2ea83d2f2503c1e2
dorian <dorianoriginaldesings@gmail.com>
2018-08-08 21:59:59 +0200

    Clase 7 -  Switch

--- Clase 7/scripts.js
+++ Clase 7/scripts.js
@@ -29,3 +29,32 @@ switch(evaluación) {
          //código
 } 
 */
+
+let num = 2;
+
+switch (num) {
+    case 1: 
+        console.log(`${num} tiene el valor 1`);
+        break;
+    case 2: 
+        console.log(`${num} tiene el valor 2`);
+        break;
+    default:
+    console.log(`${num} no vale ni 1 ni 2`);
+}
+
+switch (num) {
+    case 1:
+    case 3:
+    case 5:
+        console.log(`${num} es impar`);
+        break;
+    case 2: 
+    case 4: 
+        console.log(`${num} es par`);
+        break;
+    default:
+    console.log(`${num} no está contemplado en los casos`);
+}
+
+/* Switch múltiple encadenado */
