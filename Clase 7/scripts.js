eddfbaddc98193b653fff0dbc3bc4098e54d3eeb
dorian <dorianoriginaldesings@gmail.com>
2018-08-08 21:57:14 +0200

    Clase 7 - Condicional switch

---
+++ Clase 7/scripts.js
@@ -0,0 +1,31 @@
+/*
+Sintaxis Simple
+switch(evaluación) {
+    case n1: 
+        //código
+    break;
+
+    case n2:
+        //código
+    break;
+    default:
+         //código
+}
+
+Sintaxis Múltiple
+switch(evaluación) {
+    case n1:
+    case n2:
+    case n3:
+    case n4:
+        //código
+    break;
+
+    case n5:
+    case n6:
+        //código
+    break;
+    default:
+         //código
+} 
+*/
