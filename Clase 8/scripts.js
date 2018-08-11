d06234227dca9b6353f5e8d0925b65ab9079413c
dorian <dorianoriginaldesings@gmail.com>
2018-08-11 17:38:14 +0200

    Operador Ternario

---
+++ Clase 8/scripts.js
@@ -0,0 +1,30 @@
+/* 
+Operador ternario
+(condición) ? true : false
+
+(condición) ? 
+    (primera sentencia,
+    segunda sentencia)
+    :
+    (primera sentencia,
+    segunda sentencia)
+*/
+
+let num = 3;
+
+/* if(num % 2 == 0) console.log(`${num} es par`);
+else console.log(`${num} es impar`); */
+
+(num % 2 == 0) ? console.log(`${num} es par`) : console.log(`${num} es impar`);
+
+(num % 2 == 0) ? 
+    (
+        console.log(`${num} es par`),
+        console.log(`${num} es par 2`)
+    )
+     : 
+    (
+        console.log(`${num} es impar`),
+        console.log(`${num} es impar`)
+    )
+
