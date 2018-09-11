9b6a973541f551394f770172b1a69a5a49d4aee6
dorian <dorianoriginaldesings@gmail.com>
2018-09-12 00:54:26 +0200

    Bucle for

---
+++ Clase 12/scripts.js
@@ -0,0 +1,25 @@
+/* Bucles */
+
+/* 
+    Determinados
+
+    bucle for
+
+    Su sintaxis se compone de 3 partes
+        Iniciación de variable
+        Número de vueltas
+        Incremento o decremento
+
+    for(let i=0;i<=10;i++){
+        Código a ejecutar
+    }
+*/
+
+let numbers = [56,14,23,37,41,59]
+
+for(let i=0;i<=numbers.length;i++){
+    console.log(`i vale ${i} y el valor de esa posición en el array es ${numbers[i]}`);
+}
+
+
+
