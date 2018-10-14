157e1654ce6329ed0f33ae4e0802c16735c76745
dorian <dorianoriginaldesings@gmail.com>
2018-10-14 22:45:14 +0200

    Funciones - Introducción

---
+++ Clase 18/scripts.js
@@ -0,0 +1,24 @@
+/* function saludar(){
+    console.log("Hola");
+} */
+
+/* const saludar = () => console.log("Hola");
+const saludarUsuario = (user) => console.log(`Hola ${user}`);
+
+saludarUsuario('Pepe')
+saludarUsuario('Marta') */
+
+/* const suma = (num1, num2) => {
+    if(num1 == 2){
+        return num1+num2
+    }
+    console.log("Esto no se va a ejecutar si entra en el if");
+    return num1
+}
+console.log(suma(7,3)); */
+
+const suma = (num1, num2) => num1+num2
+
+let result = suma(3,6)
+
+console.log(result);
