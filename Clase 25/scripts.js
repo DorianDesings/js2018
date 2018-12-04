0d95ef9a63fca4a57edaccabc556deef535c7537
Dorian <dorianweb86@gmail.com>
2018-12-04 04:50:29 +0100

    Atributos y clases

---
+++ Clase 25/scripts.js
@@ -0,0 +1,39 @@
+/* 
+Atributos
+    element.getAttribute('attribute')
+    element.setAttribute('attribute', value)
+
+Clases
+    element.classList.add('class','class',...)
+    element.classList.remove('class','class',...)
+    element.classList.contains('class')
+    element.classList.replace('oldClass', newClass)
+    
+    element.classList.toggle('class'[,force])
+atributos directos
+    id
+    value
+*/
+
+const title = document.getElementById('title')
+const name = document.getElementById('name')
+
+//console.log(name.getAttribute('type'))
+//name.setAttribute('type','date')
+
+//title.classList.add('main-title','other-title')
+//title.classList.remove('title')
+
+/* if(title.classList.contains('title')) console.log('Title tiene la clase title')
+else console.log('Title no tiene la clase title') */
+
+/* title.classList.replace('title','main-title') */
+
+/* console.log(title.innerHTML)
+console.log(title.textContent) */
+console.log(name.value)
+/* console.log(title)
+console.log(name) */
+
+
+
