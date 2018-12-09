cd4a86499974f7116b93cc0d9da3aee3b1b69ee1
Dorian <dorianweb86@gmail.com>
2018-12-09 16:36:59 +0100

    Eventos - introducción

---
+++ Clase 26/scripts.js
@@ -0,0 +1,61 @@
+/* 
+Eventos de ratón:
+    click - cuando pulsamos el botón izquierdo del ratón
+    dblclick - cuando pulsamos dos veces seguidas el botón izquierdo del ratón
+    mouseenter - cuando entramos en la zona que tiene el evento
+    mouseleave - cuando salimos de la zona que tiene el evento
+    mousedown - cuando pulsamos el boton izquierdo del ratón
+    mouseup - cuando soltamos el boton izquierdo del ratón
+    mousemove - cuando movemos el ratón
+
+Eventos de teclado:
+    keydown - cuando pulsamos una tecla
+    keyup - cuando soltamos una tecla
+    keypress - cuando pulsamos una tecla y no la soltamos
+*/
+
+const button = document.getElementById('button')
+const input = document.getElementById('input')
+const box = document.getElementById('box')
+
+/* button.addEventListener('click', () => {
+    console.log('CLICK')
+}) */
+
+/* button.addEventListener('dblclick',()=> {
+    console.log('DOBLE CLICK')
+}) */
+
+/* box.addEventListener('mouseenter', () => {
+    box.classList.replace('red', 'green')
+
+})
+
+box.addEventListener('mouseleave', () => {
+    box.classList.replace('green', 'red')
+}) */
+
+/* box.addEventListener('mousedown', () => {
+    console.log('HAS PULSADO EN LA CAJA');
+})
+
+box.addEventListener('mouseup', () => {
+  console.log('HAS SOLTADO EL BOTON IZQUIERDO EN LA CAJA')
+}) */
+
+/* box.addEventListener('mousemove', () => {
+    console.log('ESTAS MOVIENDO EL RATON EN LA CAJA');
+}) */
+
+/* input.addEventListener('keydown', () => {
+    console.log('HAS PULSADO UNA TECLA');
+})
+
+input.addEventListener('keyup', () => {
+  console.log('HAS SOLTADO UNA TECLA')
+})
+
+input.addEventListener('keypress', () => {
+  console.log('ESTÁS PULSANDO UNA TECLA')
+}) */
+
