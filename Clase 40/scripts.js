025724c280a9c8985aedeba4169cbea9705666f9
sheilitamr <sheilitamr@gmail.com>
2019-06-23 01:38:31 +0200

    async/await

---
+++ Clase 40/scripts.js
@@ -0,0 +1,46 @@
+/* const getName = async () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Dorian')
+        }, 1500)
+    })
+}
+
+const sayHello = async () => {
+    const name = await getName()
+    return `Hello ${name}`
+}
+
+sayHello().then(res=>console.log(res)) */
+
+const users = [{ id: 1, name: 'Dorian' }, { id: 2, name: 'Laura' }, { id: 3, name: 'Carlos' }]
+
+const emails = [{ id: 1, email: 'dorian@gmail.com' }, { id: 2, email: 'laura@gmail.com' }]
+
+const getUser = async (id) => {
+    const user = users.find(user => user.id == id)
+    if (!user) throw new Error(`Doesn't exist an user with id ${id}`)
+    else return user
+}
+
+const getEmail = async (user) => {
+    const email = emails.find(email => email.id == user.id)
+    if (!email) throw new Error(`${user.name} hasn't email`)
+    else return ({
+        id: user.id,
+        name: user.name,
+        email: email.email
+    })
+}
+
+const getInfo = async (id) => {
+    try {
+        const user = await getUser(id)
+        const res = await getEmail(user)
+        return `${user.name} email is ${res.email}`
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getInfo(3).then(res => console.log(res))
