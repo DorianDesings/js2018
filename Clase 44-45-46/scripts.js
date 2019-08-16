3cd5f176cea7978aafd630f7213989467cd8ae2b
DorianDesings <dorianoriginaldesings@gmail.com>
2019-08-16 16:45:11 +0200

    Expresiones Regulares I, II y III

--- Clase 44-45-46/scripts.js
+++ Clase 44-45-46/scripts.js
@@ -1,6 +1,7 @@
 /*
     Expresiones regulares:
         Son una secuencia de caracteres que forma un patrón de búsqueda, principalmente utilizada para la búsqueda de patrones de cadenas de caracteres u operaciones de sustituciones.
+        https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Regular_Expressions
 
     Página para evaluar expresiones regulares: https://regex101.com/
 
@@ -24,6 +25,23 @@
 
         -Cadenas completas:
             Para establecer una cadena completa debe ir entre paréntesis, si queremos más palabras irán separadas por un pipe. (lorem|amet) es válida la palabra "lorem" y la palabra "amet"
+
+    Delimitadores:
+        ^ Antes de este símbolo no puede haber nada
+        $ Después de este símbolo no puede haber nada
+        ^hola$
+    Cantidad:
+        - llaves: lo que está antes tiene que aparecer la cantidad exacta de veces. Hay tres combinaciones posibles. 
+            {n} Se tiene que repetir n veces
+            {n,m} Se tiene que repetir entre n y m veces, ambas incluidas.
+            {n,} Se tiene que repetir como mínimo n veces y sin máximo
+        ^[a-zA-Z]{1,3}@{1}$
+        -asterisco: Lo que está antes del asterisco puede estar, puede no estar y se puede repetir.  .*@.*\..*
+        -interrogación: Lo que está antes de la interrogación puede no estar, pero si está solo puede aparecer una vez.
+            ^[ae]?$ 
+        - operador +: lo que está antes del + tiene que estár una vez como mínimo
+        A-[0-9]+
+       
 
 */
 
