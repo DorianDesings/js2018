0861e6150fad21487286cb6924e885a9f1506f62
DorianDesings <dorianoriginaldesings@gmail.com>
2019-08-25 01:34:23 +0200

    Expresiones Regulares I, II, III y IV

--- Clase 44-45-46-47/scripts.js
+++ Clase 44-45-46-47/scripts.js
@@ -41,6 +41,14 @@
             ^[ae]?$ 
         - operador +: lo que está antes del + tiene que estár una vez como mínimo
         A-[0-9]+
+    
+    Caracteres:
+        \s: Coincide con un carácter de espacio, entre ellos incluidos espacio, tab, salto de página, salto de linea y retorno de carro. ^[a-zA-Z]+\s[a-zA-Z]+$
+        \S: Coincide con todo menos caracteres de espacio ^\S{5}$
+        \d: Coincide con un carácter de número. Equivalente a [0-9] ^\d{5}$
+        \D: Coincide con cualquier carácter no numérico. Equivalente a [^0-9] ^\D{5}$
+        \w: Coincide con cualquier carácter alfanumérico, incluyendo el guión bajo. Equivalente a [A-Za-z0-9_] ^\w+@$
+        \W: Coincide con todo menos caracteres de palabra. ^\W+$
        
 
 */
