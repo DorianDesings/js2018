b6fe2581cb85aeac06eb8ac14303ed19faa86343
DorianDesings <dorianoriginaldesings@gmail.com>
2019-11-10 13:06:52 +0100

    Drag & Drop + File

---
+++ Clase 54/scripts.js
@@ -0,0 +1,24 @@
+const fileInput = document.getElementById('file')
+const dropZone = document.getElementById('drop-zone')
+
+dropZone.addEventListener('click', () => fileInput.click())
+
+dropZone.addEventListener('dragover', (e) => {
+    e.preventDefault()
+    dropZone.classList.add('drop-zone--active')
+})
+
+dropZone.addEventListener('dragleave', (e) => {
+    e.preventDefault()
+    dropZone.classList.remove('drop-zone--active')
+})
+
+dropZone.addEventListener('drop', (e) => {
+    e.preventDefault()
+    fileInput.files = e.dataTransfer.files
+    console.log(fileInput.files);
+})
+
+fileInput.addEventListener('change', (e) => {
+    console.log(fileInput.files);
+})
