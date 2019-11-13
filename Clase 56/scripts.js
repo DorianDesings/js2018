cf3fb4c085617800c1210da7c7ca808bdb1e105a
DorianDesings <dorianoriginaldesings@gmail.com>
2019-11-13 18:11:17 +0100

    IndexedDB II - Añadiendo registros

---
+++ Clase 56/scripts.js
@@ -0,0 +1,39 @@
+const indexedDB = window.indexedDB
+const form = document.getElementById('form')
+
+if (indexedDB && form) {
+    let db
+    const request = indexedDB.open('tasksList', 1)
+
+    request.onsuccess = () => {
+        db = request.result
+        console.log('OPEN', db)
+    }
+
+    request.onupgradeneeded = (e) => {
+        db = e.target.result
+        console.log('Create', db)
+        const objectStore = db.createObjectStore('tasks', {
+            autoIncrement: true
+        })
+    }
+
+    request.onerror = (error) => {
+        console.log('Error', error)
+    }
+
+    const addData = (data) => {
+        const transaction = db.transaction(['tasks'], 'readwrite')
+        const objectStore = transaction.objectStore('tasks')
+        const request = objectStore.add(data)
+    }
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const data = {
+            taskTitle: e.target.task.value,
+            taskPriority: e.target.priority.value
+        }
+        addData(data)
+    })
+}
