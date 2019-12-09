b4b9570e333617723df468215691cf508d5b27db
DorianDesings <dorianoriginaldesings@gmail.com>
2019-12-09 18:04:19 +0100

    Geolocation

---
+++ Clase 65/scripts.js
@@ -0,0 +1,25 @@
+/* 
+    https://developer.mozilla.org/es/docs/Web/API/NavigatorGeolocation/geolocation
+*/
+
+
+const button = document.getElementById('button')
+
+button.addEventListener('click', () => {
+    const geolocation = navigator.geolocation
+
+    geolocation.getCurrentPosition(getPosition, error, options)
+})
+
+const options = {
+    enableHightAccuracy: true,
+    timeout: 5000,
+    maximunAge: 0
+}
+
+const getPosition = (position) => {
+    console.log(position)
+
+}
+
+const error = (error) => console.log(error)
