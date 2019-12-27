45bb9ab57817697ba73b5eb51bab9c4e238453db
DorianDesings <dorianoriginaldesings@gmail.com>
2019-12-27 22:39:39 +0100

    Match Media

--- Clase 66/scripts.js
+++ Clase 66/scripts.js
@@ -3,23 +3,25 @@
 
     mql = window.matchMedia(mediaQueryString)
 
+    mql viene de media query list, que es el objeto que se crea con el método matchMedia()
     mediaQueryString es cualquier media query válida en CSS
 */
 
+const title = document.getElementById('title')
+
+const mql = matchMedia('(min-width:400px) and (orientation: landscape)')
+
 const applyMatchMedia = mql => {
     mql.matches ?
-        //NO HACER ESTO NUNCA :)
+        //NO HACER ESTO NUNCA
         document.body.style.backgroundColor = 'red'
+        //ESTO SI
+        // title.classList.add('clase que sea')
         :
-        //NO HACER ESTO NUNCA :)
+        //NO HACER ESTO NUNCA
         document.body.style.backgroundColor = 'royalblue'
 }
 
-mql = matchMedia('(min-width:800px)')
+addEventListener('resize', () => applyMatchMedia(mql))
+addEventListener('DOMContentLoaded', () => applyMatchMedia(mql))
 
-addEventListener('resize', (e) => {
-    // if (innerWidth == '800') console.log(innerWidth)
-    applyMatchMedia(mql)
-})
-
-applyMatchMedia(mql)
