32a70a104c6c300cd41e7db04a035fb5e451cfca
DorianDesings <dorianoriginaldesings@gmail.com>
2019-12-30 06:22:12 +0100

    Debugging

---
+++ Clase 68/scripts.js
@@ -0,0 +1,17 @@
+/* 
+    Tutorial chrome https://developers.google.com/web/tools/chrome-devtools/javascript?hl=es
+*/
+
+const form = document.getElementById('form')
+const resultElement = document.getElementById('result')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if (e.target.number1.value != '' && e.target.number2.value != '') {
+        const number1 = parseInt(e.target.number1.value)
+        const number2 = parseInt(e.target.number2.value)
+        const result = number1 + number2
+        resultElement.textContent = `${number1} + ${number2} = ${result}`
+        e.target.reset()
+    }
+})
